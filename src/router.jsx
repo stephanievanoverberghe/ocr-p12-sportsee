fc76378c9afd6f7b63db0632a96d1110da942af6
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-12-10 10:55:34 +0100

    fix: update

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Profile from './pages/Profile/index';
 import Error from './pages/ErrorPage';
